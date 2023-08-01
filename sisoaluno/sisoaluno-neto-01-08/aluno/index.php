390cb9a2b9d86addaed8621a66da92ca74238db2
adilton-neto <130432269+adilton-neto@users.noreply.github.com>
2023-08-01 11:47:33 -0300

    Add files via upload

---
+++ sisoaluno/sisoaluno-neto-01-08/aluno/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>menu principal</title>
+</head>
+<body>
+
+<div class="box">
+      <div class="heater">
+<h1>Sisopro IF</h1>
+
+      </div>
+      <div class="conteudo">
+      <div class="conteudo-dentro conteudo-lado1">
+        
+      <!--<a  class="btn" href="cadaluno.php"> <input class="botao" type="submit" value="aluno"> </a>-->
+             <button onclick="document.location='listaalunos.php'" class="btn" type="button" value="aluno"> aluno </button>
+             <button onclick="document.location='../profeesor/listaalunos.php'" class="btn" type="button" value="professor">professor</button>
+             <button onclick="document.location='../disciplinas/listaalunos.php'" class="btn" type="button" value="disciplina">disciplina</button>
+
+      </div>
+      <div class="conteudo-dentro conteudo-lado2">
+      <h1 class="tt-lado2" >Sobre o sistema</h1>
+           <p class="texto-lado2">O sistema cadastra alunos, professores e disciplinas, na tela inicial ao clicar em algum botao iremos listar o indicado, mostrando-os os cadastrados em nossos sistemas. Voce usuario tambem pode se cadastrar, cadastrar novas disciplinas ou outras pessoas! </p>
+
+
+
+      </div>
+      </div>
+      <div class="footer">
+        <h2>@footer</h2>
+      </div>
+
+
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   <!-- <button class="button"><a href="cadaluno.php">cadastrar</a></button>-->
+    
+  
+
+</body>
+</html>
---
+++ sisoaluno/sisoaluno-neto-01-08/disciplinas/altaluno.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+    require_once('conexao.php');
+
+   $id = $_POST['id'];
+
+   ##sql para selecionar apens um aluno
+   $sql = "SELECT * FROM disciplina where id= :id";
+   
+   # junta o sql a conexao do banco
+   $retorno = $conexao->prepare($sql);
+
+   ##diz o paramentro e o tipo  do paramentros
+   $retorno->bindParam(':id',$id, PDO::PARAM_INT);
+
+   #executa a estrutura no banco
+   $retorno->execute();
+
+  #transforma o retorno em array
+   $array_retorno=$retorno->fetch();
+   
+   ##armazena retorno em variaveis
+   $nome = $array_retorno['nomedisciplina'];
+   $idade = $array_retorno['idprofessor'];
+   $datanascimento = $array_retorno['ch'];
+   $endereco = $array_retorno['semestre'];
+
+
+?>
+<div class="heater heater-1">
+    <h1> alterar DISCIPLINAS cadastrados</h1>
+</div>
+
+<div class="box-1">
+
+
+  <div class="formulario"> 
+  <form class="form-cadaluno" method="POST" action="crudaluno.php">
+  <div class="intem-form item-1">
+  <label for="">Nome aluno</label>
+        <input type="text" name="nomedisciplina" id="" value=<?php echo $nomedisciplina?> >
+        </div>
+        <div class="intem-form">
+        <label for="">Idade</label>
+        <input type="text" name="idprofessor" id="" value=<?php echo $idprofessor ?> >
+        </div>
+        <div class="intem-form">
+        <label for="">Data de Nascimento</label>
+        <input type="date" name="ch" id="" value=<?php echo $ch ?> >
+        </div>
+        <div class="intem-form">
+        <label for="">endereço</label>
+        <input type="text" name="semestre" id="" value=<?php echo $semestre ?> >
+        </div>
+
+        <input type="hidden" name="id" id="" value=<?php echo $id ?> >
+        
+        <input  class="btn-footer-cadastrar cadastrar-1 btn-alterar"  type="submit" name="update" value="Alterar">
+
+
+  </form>
+          </div>
+             </div>
+
+
+
+
+</body>
+</html>
